961b029733a03738288a716413c0e64bc483ef6f
HephzibahAbigail <abigailafolayemi@gmail.com>
2023-09-12 09:00:12 -0700

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-
 /**
-* main - program start
-* Return: Zero
+* main - Start of program
+* Return: Return 0
 */
-
-
 int main(void)
 {
-int num_x;
-for (num_x = 48; num_x <= 57; num_x++)
+char x;
+for (x = 97; x <= 122; x++)
 {
-putchar(num_x);
-if (num_x <= 56)
-{
-putchar(44);
-putchar(32);
-}
+putchar(x);
 }
 putchar(10);
 return (0);
