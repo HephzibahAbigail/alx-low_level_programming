5ed60a3f0052c7f0719a06d5378a0936dba3bf9d
HephzibahAbigail <abigailafolayemi@gmail.com>
2023-09-18 19:38:09 -0700

    long aray

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,30 +9,29 @@
 
 int _atoi(char *s)
 {
-int i, value, sign;
-char c;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-i = 0;
-value = 0;
-sign = -1;
-while (s[i] != '\0')
-{
-c = s[i];
-if (c == '-')
-sign *= -1;
-else if (c >= '0' && c <= '9')
-{
-if (value < 0)
-value = value * 10 - (c - '0');
-else
-value = (c - '0') * -1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
-}
-i++;
-}
-if (sign < 0)
-value *= -1;
-
-return (0);
+	while (s[c])
+	{
+	if (s[c] == 45)
+	{
+	min *= -1;
+	}
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	isi = 1;
+	ni = (ni * 10) + (s[c] - '0');
+	c++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= min;
+	return (ni);
 }
